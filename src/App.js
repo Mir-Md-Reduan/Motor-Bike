f8373093924ead6d0d55ff5079e035928410dd72
Mir-Md-Reduan <mhoqu091050@bsee.uiu.ac.bd>
2021-11-26 20:26:17 +0600

    Deleted .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Hello From React portfolio app .</h2>
+      <h2>Hello From React portfolio app</h2>
     </div>
   );
 }
